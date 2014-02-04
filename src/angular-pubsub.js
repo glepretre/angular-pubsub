adbd54db790737903cef1e20e96b37bdc4b4b5b8
Gilles Lepretre <gilles.lepretre@lim.eu>
2014-02-04 18:00:00 +0100

    First commit

---
+++ src/angular-pubsub.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var angularPubsub = angular.module('angularPubsub', []);
+
+angularPubsub.factory('angularPubsub', function(){
+  var channels = {};
+  return {
+    publish: function(topic) {
+      var args = Array.prototype.slice.call(arguments, 1);
+      channels[topic] && channels[topic].forEach(function(callback) {
+        callback.apply(null, args);
+      });
+    },
+
+    subscribe: function(topic, callback) {
+      if(!(callback instanceof Function)){
+        throw new Error('callback must be a function');
+      }
+      if(!channels[topic]) {
+        channels[topic] = [];
+      }
+      channels[topic].push(callback);
+    },
+
+    unsubscribe: function(topic, callback) {
+      channels[topic] && channels[topic].forEach(function(value, index){
+        if(value === callback) {
+          channels[topic].splice(index, 1);
+        }
+      });
+    }
+  }
+});
