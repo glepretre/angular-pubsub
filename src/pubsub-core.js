78ccaa27abc6fddf6bec549ceb2b3599c14ce126
Gilles Lepretre <gilles.lepretre@lim.eu>
2015-09-24 09:24:28 +0200

    Remove useless comment

--- src/pubsub-core.js
+++ src/pubsub-core.js
@@ -15,9 +15,6 @@
     root.pubsubCore = factory();
   }
 }(this, function() {
-  // Just return a value to define the module export.
-  // This example returns an object, but the module
-  // can return a function as the exported value.
   'use strict';
   return function() {
     var channels = {};
