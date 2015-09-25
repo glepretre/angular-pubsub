0ef7c894cb5eeaf53ca1b5ce507a576d43caa590
Gilles Lepretre <gilles.lepretre@lim.eu>
2015-09-25 14:38:47 +0200

    Disable build minification

--- config/build.conf.js
+++ config/build.conf.js
@@ -2,6 +2,7 @@
   mainConfigFile: './require.conf.js',
   name:'angular-pubsub',
   out: '../dist/angular-pubsub.js',
+  optimize: 'none',
   useStrict: true,
   paths: {
     'angular': 'empty:'
--- dist/angular-pubsub.js
+++ dist/angular-pubsub.js
@@ -1,1 +1,87 @@
-(function(e,t){"use strict";typeof define=="function"&&define.amd?define("pubsub-core",[],t):typeof module=="object"&&module.exports?module.exports=t():e.pubsubCore=t()})(this,function(){"use strict";return function(){var e={};return{publish:function(t){var n=Array.prototype.slice.call(arguments,1);if(!e[t])return;e[t].forEach(function(e){e.apply(null,n)})},subscribe:function(t,n){if(!(n instanceof Function))throw new Error("callback must be a function");e[t]||(e[t]=[]),e[t].push(n)},unsubscribe:function(t,n){if(!e[t])return;e[t].forEach(function(r,i){r===n&&e[t].splice(i,1)})}}}}),function(e,t){"use strict";typeof define=="function"&&define.amd?define("angular-pubsub",["angular","pubsub-core"],t):e.angularPubsub=t(e.angular,e.pubsubCore)}(this,function(e,t){"use strict";var n=e.module("angularPubsub",[]);return n.factory("PubSub",t)});
+/*
+ * UMD (Universal Module Definition) pattern
+ * https://github.com/umdjs/umd
+ * returnExports snippet
+ * https://github.com/umdjs/umd/blob/master/returnExports.js
+ */
+(function(root, factory) {
+  'use strict';
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define('pubsub-core',[], factory);
+  }
+  else if (typeof module === 'object' && module.exports) {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory();
+  }
+  else {
+    // Browser globals (root is window)
+    root.pubsubCore = factory();
+  }
+}(this, function() {
+  'use strict';
+  return function() {
+    var channels = {};
+    return {
+      publish: function(topic) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        if (!channels[topic]) {
+          return;
+        }
+
+        channels[topic].forEach(function(callback) {
+          callback.apply(null, args);
+        });
+      },
+
+      subscribe: function(topic, callback) {
+        if (!(callback instanceof Function)){
+          throw new Error('callback must be a function');
+        }
+        if (!channels[topic]) {
+          channels[topic] = [];
+        }
+        channels[topic].push(callback);
+      },
+
+      unsubscribe: function(topic, callback) {
+        if (!channels[topic]) {
+          return;
+        }
+
+        channels[topic].forEach(function(value, index) {
+          if (value === callback) {
+            channels[topic].splice(index, 1);
+          }
+        });
+      }
+    };
+  };
+}));
+
+/*
+ * UMD (Universal Module Definition) pattern
+ * https://github.com/umdjs/umd
+ * amdWeb snippet
+ * https://github.com/umdjs/umd/blob/master/amdWeb.js
+ */
+(function(root, factory) {
+  'use strict';
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define('angular-pubsub',['angular', 'pubsub-core'], factory);
+  }
+  else {
+    // Browser globals
+    root.angularPubsub = factory(root.angular,
+                                 root.pubsubCore);
+  }
+}(this, function(angular, pubsubCore) {
+  'use strict';
+  var angularPubsub = angular.module('angularPubsub', []);
+
+  return angularPubsub.factory('PubSub', pubsubCore);
+}));
+
